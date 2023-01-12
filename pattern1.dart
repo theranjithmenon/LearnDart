0241d2be9bd340a43580a06755e309e5f9a449e2
Ranjith Menon <60739917+theranjithmenon@users.noreply.github.com>
2023-01-12 12:11:53 +0530

    Pattern

--- pattern1.dart
+++ pattern1.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 void main() {
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j <= i; j++) {
-      stdout.write('* ');
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 0; j < i; j++) {
+      stdout.write(i);
     }
     stdout.writeln();
   }
